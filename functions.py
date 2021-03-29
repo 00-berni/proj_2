7f8128a9d7380a45e0db6c5602436ae8c551ef61
bernardo <bernardo.vettori@outlook.it>
2021-03-29 21:20:26 +0200

    niente di che

--- functions.py
+++ functions.py
@@ -8,14 +8,23 @@
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import random as rdm
 from datetime import datetime
+
+##* star class: variable type for the star parameters
+class star:
+	def __init__(self, lum, x ,y):
+		self.lum = lum		# star luminosity value
+		self.x = x				# star x coordinate
+		self.y = y				# star y coordinate
 
 ##* Function for the initialization of the fieldfrom initial mass function
 #	param	N	dimension of the field
 #	param	IMF	Initial Mass Function
 #
 #	return F	N*N field matrix with sources
+#!	capire come usare IMF
 def initialize(N,IMF):
 	# Definition of the empty field matrix
 	# Each pixel rapresents detected luminosity of that part of sky
@@ -24,3 +33,8 @@ def initialize(N,IMF):
 	# in order to change it everytime user runs the script
 	rdm.seed(datetime.now())
 
+
+
+##* Plotting function
+def plotting():
+	from import 
