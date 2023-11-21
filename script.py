15c2da792f90d2b76e787403e2c2c54a9d7703c4
00-berni <bernardo.vettori@outlook.it>
2023-11-21 22:27:18 +0100

    Display

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import skysimulation.field as field
 import skysimulation.restoration as restore
 
@@ -23,3 +24,10 @@ if __name__ == '__main__':
     field.fast_image(F_bsd)
     extr = restore.object_isolation(F_bsd,max(back,dark.max()),objnum=objnum,reshape=True,reshape_corr=True)
     ext_kernel = restore.kernel_extimation(extr,back,ndet,len(F),True)
+    F_ext = restore.LR_deconvolution(F_bsd,ext_kernel,back,ndet)
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(F_bsd,norm='log')
+    plt.subplot(1,2,2)
+    plt.imshow(F_ext,norm='log')
+    plt.show()
