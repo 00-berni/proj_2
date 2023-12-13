46af61e9bac140094b02b544d9a71e3962ef5c1b
00-berni <bernardo.vettori@outlook.it>
2023-12-13 12:14:31 +0100

    Updates

--- test_rl.py
+++ test_rl.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skysimulation import field
+from skysimulation import display
 import skysimulation.restoration as restore
 
 
@@ -20,12 +21,14 @@ import skysimulation.restoration as restore
 #     field.fast_image(Ff, v=1)
 #     return F, Ff
 
+K = field.K
+
 def initialize(dim: int, num: int, display_fig: bool = False, **kwargs):
     beta = field.BETA
-    masses = np.linspace(field.MIN_m,field.MAX_m, num)
+    masses = np.linspace(field.MIN_m,8, num)
     lums = masses**beta
     F = np.zeros((dim,dim))
-    ynum = 3
+    ynum = 5
     xnum = num // ynum + num % ynum
     print(xnum,ynum)
     x = np.array([i*dim // (xnum+1) for i in range(1,xnum+1)]*ynum)
@@ -34,48 +37,71 @@ def initialize(dim: int, num: int, display_fig: bool = False, **kwargs):
     if num != xnum*ynum: 
         diff = xnum*ynum - num
         lums = np.append(lums,[0]*diff)
-    F[y,x] = lums
+    F[y,x] = lums * K
 
     if display_fig:
         if 'title' not in kwargs:
             kwargs['title'] = f'Inizialized Field\nMass range [{masses.min():.1f}, {masses.max():.1f}]'
             field.fast_image(F,**kwargs)
-    return F    
+    return F, masses, (x,y)    
 
 
-def add_effects(F,back,atm,det,**kwargs):
+def add_effects(F,masses,coor,back,atm,det,**kwargs):
     N = len(F)
     figure = kwargs['figure']
     norm = kwargs['norm']
     v = kwargs['v']
     print('\nBackground')
-    Fb = F + field.noise(back,N,True,figure,norm=norm,v=v,title='Background')
-    field.fast_image(Fb,v=v,norm=norm,title='F + B')
+    Fb = F + field.noise(back,dim=N,infos=True,display_fig=figure,norm=norm,v=v,title='Background')
+    if figure:
+        field.fast_image(Fb,v=v,norm=norm,title='F + B')
     print('\nSeeing')
-    Fs = field.atm_seeing(F,atm,figure,title='Bare Atm Seeing',norm=norm,v=v)
+    Fs = field.atm_seeing(F,atm,display_fig=figure,title='Bare Atm Seeing',norm=norm,v=v)
     Fbs = field.atm_seeing(Fb,atm,figure,norm=norm,v=v)
     print('\nNoise')
-    Fbsn = Fbs + field.noise(det,N,True,figure,title='Noise',norm=norm,v=v)
+    Fbsn = Fbs + field.noise(det,dim=N,infos=True,display_fig=figure,title='Noise',norm=norm,v=v)
+    if figure:
+        field.fast_image(Fbsn,v=0,norm='linear',title='Field')
+
+    fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2,figsize=(15,15))
+    plt.subplots_adjust(wspace=0.01)
+    fig.suptitle('Initialization Process',fontsize=20)
+
+    ax1.set_title('Initial Field',fontsize=14)
+    display.field_image(fig,ax1,F,v=2,norm=norm)
+    x,y = coor
+    for i in range(len(masses)):
+        ax1.annotate(f'{masses[i]:.1f}',(x[i],y[i]),(x[i]-4,y[i]-3),fontsize=12)
+
+    ax2.set_title('Field and Background',fontsize=14)
+    display.field_image(fig,ax2,Fb,v=v,norm=norm)
     
-    field.fast_image(Fbsn,v=0,norm='linear',title='Field')
+    ax3.set_title('Seeing',fontsize=14)
+    display.field_image(fig,ax3,Fbs,v=v,norm=norm)
+    
+    ax4.set_title('Field and Noise',fontsize=14)
+    display.field_image(fig,ax4,Fbsn,v=v,norm=norm)
+    
+    plt.show()
+
 
     return Fbsn
 
 
 if __name__ == '__main__':
     N = 100
-    M = 15
-    figure = True
-    norm = 'linear'
-    v = 1
+    M = 20
+    figure = False
+    norm = 'log'
+    v = 0
     print('Initializing')
-    F = initialize(N,M,display_fig=figure,v=v,norm=norm)
+    F, masses, coor = initialize(N,M,display_fig=figure,v=1,norm=norm)
     
     print('\nI RUN')
     back = field.BACK_PARAM
     det = field.NOISE_PARAM
     atm = field.SEEING_SIGMA
-    I = add_effects(F,back,atm,det,figure=figure,norm=norm,v=v)
+    I = add_effects(F,masses,coor,back,atm,det,figure=figure,norm=norm,v=v)
 
     # print('\nII RUN')
     # back = ('Gaussian',(0.2, field.BACK_SIGMA))
