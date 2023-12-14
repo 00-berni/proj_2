913afa9df37e1891c7bf6a64658a0be1b65aed54
00-berni <bernardo.vettori@outlook.it>
2023-12-14 17:25:19 +0100

    Updates

--- script.py
+++ script.py
@@ -6,9 +6,11 @@ import skysimulation.restoration as restore
 if __name__ == '__main__':
     N = 100
     M = 4500
-    figure = True
+    figure = False
     norm = 'log'
     back = field.BACK_PARAM
     det = field.NOISE_PARAM
     S, I = field.field_builder(N,M,back_param=back,det_param=det,display_fig=figure,norm=norm)
-
+    S.plot_info(field.ALPHA,field.BETA)
+    n = restore.bkg_est(I,True)
+    print(n/field.K)
