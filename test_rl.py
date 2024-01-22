703310b8af652c2d05d62313c245f42c56b50fb2
00-berni <bernardo.vettori@outlook.it>
2024-01-22 17:24:20 +0100

    Change the way test is performed

--- test_rl.py
+++ test_rl.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from skysimulation import NDArray
 from skysimulation import field
 from skysimulation import display
 import skysimulation.restoration as restore
@@ -8,20 +9,25 @@ import skysimulation.restoration as restore
 K = field.K
 st_pos = 20
 
-def initialize(dim: int, num: int,max_mass: float | int = 8, display_fig: bool = False, **kwargs):
+def initialize(dim: int, num: int, max_mass: float | int = 8, pos: str = 'grid', set_pos: NDArray | None = None, display_fig: bool = False, **kwargs):
     beta = field.BETA
     masses = np.linspace(field.MIN_m,max_mass,num)
     lums = masses**beta
     F = np.zeros((dim,dim))
-    ynum = 5
-    xnum = num // ynum + num % ynum
-    print(xnum,ynum)
-    x = np.array([i*dim // (xnum+1) for i in range(1,xnum+1)]*ynum)
-    y = np.sort(np.array([(i+1)*dim // (ynum+1) for i in range(ynum)]*xnum))
-    print(len(x),len(y))
-    if num != xnum*ynum: 
-        diff = xnum*ynum - num
-        lums = np.append(lums,[0]*diff)
+    if pos == 'grid' or (pos == 'set' and set_pos is None):
+        ynum = 5
+        xnum = num // ynum + num % ynum
+        print(xnum,ynum)
+        x = np.array([i*dim // (xnum+1) for i in range(1,xnum+1)]*ynum)
+        y = np.sort(np.array([(i+1)*dim // (ynum+1) for i in range(ynum)]*xnum))
+        print(len(x),len(y))
+        if num != xnum*ynum: 
+            diff = xnum*ynum - num
+            lums = np.append(lums,[0]*diff)
+    elif pos == 'random':
+        y,x = field.star_location(num,dim,overlap=False)
+    elif pos == 'set':
+        y,x = set_pos
     F[y,x] = lums * K
     # F[st_pos,st_pos] = 4**beta * K
     # F[5,-1] = 8.6**beta * K
@@ -30,10 +36,10 @@ def initialize(dim: int, num: int,max_mass: float | int = 8, display_fig: bool =
         if 'title' not in kwargs:
             kwargs['title'] = f'Inizialized Field\nMass range [{masses.min():.1f}, {masses.max():.1f}]'
             field.fast_image(F,**kwargs)
-    return F, masses, (x,y)    
+    return F, masses, (y,x)    
 
 
-def add_effects(F,masses,coor,back,atm,det,**kwargs):
+def add_effects(F,masses,coor,back,atm,det,pos_mode: str = 'grid',**kwargs):
     N = len(F)
     if 'results' not in kwargs: kwargs['results'] = False
     if 'figure' not in kwargs: kwargs['figure'] = False
@@ -62,8 +68,9 @@ def add_effects(F,masses,coor,back,atm,det,**kwargs):
         ax1.set_title('Initial Field',fontsize=14)
         display.field_image(fig,ax1,F,v=2,norm='log')
         x,y = coor
-        for i in range(len(masses)):
-            ax1.annotate(f'{masses[i]:.1f}',(x[i],y[i]),(x[i]-4,y[i]+3),fontsize=12)
+        if pos_mode == 'grid':
+            for i in range(len(masses)):
+                ax1.annotate(f'{masses[i]:.1f}',(x[i],y[i]),(x[i]-4,y[i]+3),fontsize=12)
 
         ax2.set_title('Field and Background',fontsize=14)
         display.field_image(fig,ax2,Fb,v=v,norm=norm)
@@ -83,12 +90,16 @@ def add_effects(F,masses,coor,back,atm,det,**kwargs):
 if __name__ == '__main__':
     N = 100
     M = 20
-    figure = False
+    figure = True
     norm = 'linear'
     v = 0
+    pos_mode = 'random'
+    set_pos = None
+    # pos_mode = 'set'
+    # set_pos = np.array([ [0]*10 + [N-1]*10, [i*4 for i in range(20)] ])
     print('Initializing')
-    max_mass = 10
-    F, masses, coor = initialize(N,M,max_mass,display_fig=figure,v=1,norm=norm)
+    max_mass = 5
+    F, masses, coor = initialize(N,M,max_mass,pos=pos_mode,set_pos=set_pos,display_fig=figure,v=1,norm=norm)
 
     print('\nI RUN')
     back = field.BACK_PARAM
@@ -99,7 +110,7 @@ if __name__ == '__main__':
     results = []
     nn = []
     for _ in range(iter):
-        I = add_effects(F,masses,coor,back,atm,det,figure=figure,norm=norm,v=v,results=True)
+        I = add_effects(F,masses,coor,back,atm,det,pos_mode=pos_mode,figure=figure,norm=norm,v=v,results=True)
         results += [I]
         n = restore.bkg_est(I,True)
         print(n)
@@ -112,39 +123,56 @@ if __name__ == '__main__':
     print('nm ',nm/K)
     print('min',min(nn)/K)
 
-    dark = restore.dark_elaboration(det)
-    bkg = nn[0]
-    I = results[0]
+    # dark = restore.dark_elaboration(det)
+    # bkg = nn[0]
+    # I = results[0]
     
-    mean_val = max(bkg,dark.mean())
-    objs, obj_pos = restore.object_isolation(I,mean_val,size=7,objnum=20,reshape=True,reshape_corr=True,sel_cond=True,display_fig=False,norm=norm)
+    # mean_val = max(bkg,dark.mean())
+    # objs, obj_pos = restore.object_isolation(I,mean_val,size=7,objnum=20,reshape=True,reshape_corr=True,sel_cond=True,display_fig=False,norm=norm)
 
-    if objs is not None:
-        try:
-            err = restore.err_estimation(I,mean_val,display_plot=True)
-        except:
-            err = None
-        kernel,(sigma, Dsigma) = restore.kernel_estimation(objs,err,N,all_results=False,display_plot=False)
+    # if objs is not None:
+    #     try:
+    #         err = restore.err_estimation(I,mean_val,display_plot=True)
+    #     except:
+    #         err = None
+    #     kernel,(sigma, Dsigma) = restore.kernel_estimation(objs,err,N,all_results=False,display_plot=False)
 
-        rec_I = restore.LR_deconvolution(I,kernel,mean_val,iter=50,sel='rl',display_fig=True)
-        print('Center Value',I[st_pos,st_pos],rec_I[st_pos,st_pos])
-        # mask = restore.mask_filter(rec_I,I,True)
-        lum, pos = restore.find_objects(rec_I,I,kernel,mean_val,sel_pos=obj_pos,display_fig=False)
-        l, Dl = lum
-        l0 = masses**field.BETA * K
-        print('MAXes',l0.max(),l.max())
-        print('MINes',l0.min(),l.min())
-        fig, ax = plt.subplots(1,1)
-        bins = np.linspace(min(l.min(),l0.min()),max(l.max(),l0.max()),len(l0)*4)
-        l_counts, bins = np.histogram(l,bins=bins)
-        l0_counts, bins = np.histogram(l0,bins=bins)
-        ax.stairs(l_counts,bins,label='estimated')
-        ax.stairs(l0_counts,bins,linestyle='dashed',label='original')
-        lm = np.linspace(masses.min()**field.BETA,masses.max()**field.BETA,6) *K
-        ax.set_xticks(lm)
-        ax.set_xticklabels([f'{(i/K)**(1/field.BETA):.1f}' for i in lm])
-        # ax.set_xscale('log')
-        ax.legend()
-        plt.show()
-    else:
-        print('[ALERT] - It is not possible to recover the field!\nTry to change parameters')
+    #     rec_I = restore.LR_deconvolution(I,kernel,mean_val,iter=50,sel='rl',display_fig=True)
+    #     mask = restore.mask_filter(rec_I,I,True)
+    #     lum, pos = restore.find_objects(rec_I,I,kernel,mean_val,sel_pos=obj_pos,display_fig=False)
+    #     l, Dl = lum - mean_val #- dark.mean()
+        
+    #     index  = pos.copy()
+    #     index0 = np.array(coor)[:,::-1]
+
+    #     min_dim = min(len(index[0]),len(index0[0]))
+    #     dx, dy = abs(index[:,:min_dim] - index0[:,:min_dim])
+    #     discr = 2
+    #     mat_pos = np.where(np.logical_and(dx<=discr,dy<=discr))[0]        
+    #     matches = len(mat_pos)
+
+    #     print('\n----------------\n')
+    #     print(f'MAX MASS - {max_mass}')
+    #     print(f'Found:\t{len(l)} / {M}')
+    #     print(f'Precision:\t{len(l)/M*100:.2f} %')
+    #     print(f'Match:\t{matches} / {M}')
+    #     print(f'Accuracy:\t{matches/len(l)*100:.2f} %')
+
+
+    #     # print('Center Value',I[st_pos,st_pos],rec_I[st_pos,st_pos])
+    #     # print('MAXes',l0.max(),l.max())
+    #     # print('MINes',l0.min(),l.min())
+    #     # fig, ax = plt.subplots(1,1)
+    #     # bins = np.linspace(min(l.min(),l0.min()),max(l.max(),l0.max()),len(l0)*4)
+    #     # l_counts, bins = np.histogram(l,bins=bins)
+    #     # l0_counts, bins = np.histogram(l0,bins=bins)
+    #     # ax.stairs(l_counts,bins,label='estimated')
+    #     # ax.stairs(l0_counts,bins,linestyle='dashed',label='original')
+    #     # lm = np.linspace(masses.min()**field.BETA,masses.max()**field.BETA,6) *K
+    #     # ax.set_xticks(lm)
+    #     # ax.set_xticklabels([f'{(i/K)**(1/field.BETA):.1f}' for i in lm])
+    #     # # ax.set_xscale('log')
+    #     # ax.legend()
+    #     # plt.show()
+    # else:
+    #     print('[ALERT] - It is not possible to recover the field!\nTry to change parameters')
