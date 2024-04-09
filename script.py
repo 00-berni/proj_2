f0fbd1a5bc276c97f5d1e8a0ef970c8561e43b81
00-berni <bernardo.vettori@outlook.it>
2024-04-09 17:44:34 +0200

    Structure Change --------------------------  - Collect all the procedure in a methods called `pipeline()`  - Store the results in a dictionary  - Provide multiple runs

--- script.py
+++ script.py
@@ -25,33 +25,75 @@ def plot_obj(obj0: NDArray) -> None:
 
     plt.show()    
 
+def pipeline(*args,**kwargs) -> dict[str, fld.Any]:
+    mass_seed, pos_seed, bkg_seed, det_seed = args
+    ### INITIALIZATION
+    # generate the field
+    S, (m_light, s_light), (m_dark, s_dark) = fld.field_builder(seed=(mass_seed,pos_seed), back_seed=bkg_seed, det_seed=det_seed,**kwargs)
+    # compute the scientific frame
+    sci_frame = m_light - m_dark
+    # compute the uncertainty
+    sigma = np.sqrt(s_light**2 + s_dark**2)
+    m_sigma = sigma.mean()
+    s_sigma = sigma.std()
+    print(f'SIGMA = {m_sigma} +- {s_sigma}')
+    # plot it
+    if kwargs['results']:
+        dpl.fast_image(sci_frame,'Scientific Frame')
 
-### INITIALIZATION
-mass_seed = None
-pos_seed  = None
-bkg_seed  = None
-det_seed  = None
-# mass_seed = fld.M_SEED
-# pos_seed  = fld.POS_SEED
-# bkg_seed  = fld.BACK_SEED
-# det_seed  = fld.NOISE_SEED
-# generate the field
-S, (m_light, s_light), (m_dark, s_dark) = fld.field_builder(seed=(mass_seed,pos_seed), back_seed=bkg_seed, det_seed=det_seed)
-# compute the scientific frame
-sci_frame = m_light - m_dark
-# compute the uncertainty
-sigma = np.sqrt(s_light**2 + s_dark**2)
-# plot it
-dpl.fast_image(sci_frame,'Scientific Frame')
+    ### RESTORING
+    # compute the average dark value
+    mean_dark = m_dark.mean()
+    # estimate background value
+    mean_bkg, sigma_bkg = rst.bkg_est(sci_frame, display_plot=False)
 
-### RESTORING
-# compute the average dark value
-mean_dark = m_dark.mean()
-# estimate background value
-mean_bkg, sigma_bkg = rst.bkg_est(sci_frame, display_plot=False)
+    ## Kernel Estimation
+    # extract objects for the kernel recovery
+    objs, errs, pos = rst.object_isolation(sci_frame, mean_bkg, sigma, size=5, sel_cond=True, corr_cond=False, display_fig=False,**kwargs)
+    # estimate kernel
+    ker_sigma, ker_Dsigma = rst.kernel_estimation(objs, errs, len(sci_frame), display_plot=False,**kwargs)
 
-## Kernel Estimation
-# extract objects for the kernel recovery
-objs, errs, pos = rst.object_isolation(sci_frame, mean_bkg, sigma, size=5, sel_cond=True, corr_cond=False, display_fig=False)
-# estimate kernel
-sigma, Dsigma = rst.kernel_estimation(objs, errs, len(sci_frame), display_plot=True)    
+    ## R-L
+    kernel = fld.Gaussian(ker_sigma).kernel()
+    rec_field = rst.LR_deconvolution(sci_frame,kernel,mean_bkg,sigma,display_fig=True)
+
+    ### STUFF
+    results = {}
+    results['frame']  = (sci_frame, sigma)        
+    results['dark']   = (m_dark, s_dark)
+    results['bkg']    = (mean_bkg, sigma_bkg)
+    results['objs']   = (objs, errs, pos)
+    results['seeing'] = (ker_sigma, ker_Dsigma)
+    results['rl']     = rec_field
+
+    return results        
+
+mass_seed = fld.M_SEED
+pos_seed  = fld.POS_SEED
+bkg_seed  = fld.BACK_SEED
+det_seed  = fld.NOISE_SEED
+
+default_res = pipeline(mass_seed, pos_seed, bkg_seed, det_seed,results=True)
+
+multiple_acq = False
+if multiple_acq:
+    iter = 3
+    m_res: list[dict] = []
+    mass_seed = None
+    pos_seed  = None
+    bkg_seed  = None
+    det_seed  = None
+    for i in range(iter):
+        m_res += [pipeline(mass_seed, pos_seed, bkg_seed, det_seed, results=False)]
+    
+    seeing = np.array([ [*r['seeing']] for r in m_res])
+    m_see  = seeing[:,0].mean()
+    Dm_see = seeing[:,0].std()
+    plt.figure()
+    plt.title(f'$\\sigma = $ {m_see:.2} $\pm$ {Dm_see:.2}')
+    plt.errorbar(np.arange(iter),seeing[:,0], seeing[:,1], fmt='.', linestyle='dashed')
+    plt.axhline(m_see,0,1,color='red',linestyle='dotted')
+    plt.axhline(m_see - Dm_see,0,1,color='orange',linestyle='dotted',alpha=0.7)
+    plt.axhline(m_see + Dm_see,0,1,color='orange',linestyle='dotted',alpha=0.7)
+    plt.axhline(3,0,1,color='black')
+    plt.show()
