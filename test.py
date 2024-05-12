49030c03dfa875e24f075e8633b0bb7b6360f703
00-berni <bernardo.vettori@outlook.it>
2024-05-12 23:10:23 +0200

    updates

--- test.py
+++ test.py
@@ -28,16 +28,17 @@ if __name__ == '__main__':
     bkg_distr = lambda mean : stf.Gaussian(sigma=mean * 20e-2, mu=mean)
     ker_sigma = 3
     kernel = stf.Gaussian(ker_sigma).kernel()
-    for mean_bkg in [5,9,10,20,30,50,70,80,100]:
+    mean_noise = 0.5
+    for mean_bkg in [0,5,10,20,30,50]:
         bkg = bkg_distr(mean_bkg)
         field = field0 + np.sqrt(bkg.field(shape=field0.shape)**2)
         fast_image(field,'Obj before convolution')
-        field = stf.field_convolve(field, kernel, bkg)
+        field = stf.field_convolve(field, kernel, bkg) #+ stf.Gaussian(sigma=mean_noise * 50e-2, mu=mean_noise).field(field0.shape) - mean_noise
         fast_image(field,f'The object for background of {mean_bkg}')
         index = stf.peak_pos(field)
         result = rst.object_check(field, index, mean_bkg, None)
         if result is not None:
-            obj, index = result
+            obj, index, dim = result
             fast_image(obj,'Object is fine')
         else:
             fast_image(field,'Object is not fine')
