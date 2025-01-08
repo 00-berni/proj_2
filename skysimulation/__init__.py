2fe85c6335e4a2f64dea58b9a4ca11eccc8a52ee
00-berni <bernardo.vettori@outlook.it>
2025-01-08 19:32:32 +0100

    Update

--- skysimulation/__init__.py
+++ skysimulation/__init__.py
@@ -1,3 +1,9 @@
-from .display import field_image
-from typing import Sequence
-from numpy.typing import NDArray
+from .display import *
+from .stuff import *
+from .field import initialize, MIN_m, MAX_m, POS_SEED, K
+from .field import from_parms_to_distr, BACK_SEED,BACK_MEAN,BACK_SIGMA
+from .field import atm_seeing, noise, SEEING_SIGMA, NOISE_SEED, NOISE_MEAN, NOISE_SIGMA
+from .restoration import bkg_est, LR_deconvolution,searching,kernel_estimation
+
+MASS_ENDS = [MIN_m, MAX_m]
+SEEDS = [POS_SEED,BACK_SEED,NOISE_SEED]
