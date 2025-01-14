4f30f436e8043420162b71fd922d586555c72f22
00-berni <bernardo.vettori@outlook.it>
2025-01-14 20:32:30 +0100

    Updates

--- skysimulation/__init__.py
+++ skysimulation/__init__.py
@@ -1,9 +1,11 @@
 from .display import *
 from .stuff import *
-from .field import initialize, MIN_m, MAX_m, POS_SEED, K
+from .field import initialize, field_builder, MIN_m, MAX_m, M_SEED, POS_SEED, K, N, M
 from .field import from_parms_to_distr, BACK_SEED,BACK_MEAN,BACK_SIGMA
 from .field import atm_seeing, noise, SEEING_SIGMA, NOISE_SEED, NOISE_MEAN, NOISE_SIGMA
-from .restoration import bkg_est, LR_deconvolution,searching,kernel_estimation
+from .restoration import bkg_est, LR_deconvolution,searching,kernel_estimation,light_recover
 
 MASS_ENDS = [MIN_m, MAX_m]
-SEEDS = [POS_SEED,BACK_SEED,NOISE_SEED]
+FRAME = {'size': N, 'stars': M, 'mass_seed': M_SEED, 'mass_range': MASS_ENDS, 'pos_seed': POS_SEED, 'norm': K}
+BACK  = {'seed': BACK_SEED, 'mean': BACK_MEAN, 'sigma': BACK_SIGMA}
+NOISE = {'seed': NOISE_SEED, 'mean': NOISE_MEAN, 'sigma': NOISE_SIGMA}
