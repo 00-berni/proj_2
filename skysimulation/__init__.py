eaf671ec0bc568d50187482d5a678e69c990487c
00-berni <bernardo.vettori@outlook.it>
2025-03-31 18:16:49 +0200

    Update

--- skysimulation/__init__.py
+++ skysimulation/__init__.py
@@ -1,6 +1,6 @@
 from .display import *
 from .stuff import *
-from .field import initialize, field_builder, MIN_m, MAX_m, M_SEED, POS_SEED, K, N, M, ALPHA, BETA
+from .field import initialize, field_builder, MIN_m, MAX_m, M_SEED, POS_SEED, K, N, M, ALPHA, BETA, Star
 from .field import from_parms_to_distr, BACK_SEED,BACK_MEAN,BACK_SIGMA
 from .field import atm_seeing, noise, SEEING_SIGMA, NOISE_SEED, NOISE_MEAN, NOISE_SIGMA
 from .restoration import bkg_est, LR_deconvolution,searching,kernel_estimation,light_recover
