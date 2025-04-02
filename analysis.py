bc456fbd904cbbd9a5761feb19f4107000bfac61
00-berni <bernardo.vettori@outlook.it>
2025-04-02 18:25:38 +0200

    To analyze data

---
+++ analysis.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import skysimulation as sky
+
+MAIN_DIR = 'default'
+
+source = sky.Star.load_data('source',MAIN_DIR)
+recover = sky.open_data('recovered',MAIN_DIR,'array')
+
+# bkg_mean = np.linspace(3.5,5.0,10)
+
+# mean_s = np.loadtxt(sky.os.path.join(sky.RESULT_DIR,'source.txt'),unpack=True)[-1]
+# mean_nart = [] 
+# mean_wart = []
+# for bkg in bkg_mean:
+#     path = sky.os.path.join(sky.RESULT_DIR,f'bkg-{bkg:.2f}.txt')
+    
