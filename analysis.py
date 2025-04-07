a8ce24ab5d4e26319e99718be6bf8e1ab4dc703d
00-berni <bernardo.vettori@outlook.it>
2025-04-07 18:19:27 +0200

    Update

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skysimulation as sky
 
+FONTSIZE = 18
+
 # MAIN_DIR = 'default'
 
 # source = sky.Star.load_data('source',MAIN_DIR)
@@ -19,36 +21,41 @@ import skysimulation as sky
 #     path = sky.os.path.join(sky.RESULT_DIR,f'bkg-{bkg:.2f}.txt')
     
 MAIN_DIR = 'multi-bkg-real'
-BKG_VALUES = [3.2,4.2,5.2,6.2,7.2,8.2,9.2,10,11,15,20]
+BKG_VALUES = np.array([3.2,4.2,5.2,6.2,7.2,8.2,9.2,10,11,15,20])
 BKG_ITER = 20
+# FILES_NAME =
 
 source = sky.Star.load_data(main_dir=MAIN_DIR)
 
 mean_sl = source.mean_lum()
 print(mean_sl)
 
-# (bkg, 20, [L, DL, X, Y],numpoints)
-mean_lightdata = np.array([[ np.mean(sky.open_data(f'bkg-{bkg:.2f}_{i:02d}',main_dir=MAIN_DIR,out_type='array')[0]) for i in range(BKG_ITER)] for bkg in BKG_VALUES])
-Dmean_lightdata = np.array([[ np.std(sky.open_data(f'bkg-{bkg:.2f}_{i:02d}',main_dir=MAIN_DIR,out_type='array')[0],ddof=1) for i in range(BKG_ITER)] for bkg in BKG_VALUES])
+# # (bkg, 20, [L, DL, X, Y],numpoints)
+# mean_lightdata = np.array([[ np.mean(sky.open_data(f'bkg-{bkg:.2f}_{i:02d}',main_dir=MAIN_DIR,out_type='array')[0]) for i in range(BKG_ITER)] for bkg in BKG_VALUES])
+# Dmean_lightdata = np.array([[ np.std(sky.open_data(f'bkg-{bkg:.2f}_{i:02d}',main_dir=MAIN_DIR,out_type='array')[0],ddof=1) for i in range(BKG_ITER)] for bkg in BKG_VALUES])
 
-print(np.mean(mean_lightdata,axis=1))
+# print(np.mean(mean_lightdata,axis=1))
 
-# for bkg in BKG_VALUES:
-#     for i in range(BKG_ITER)
-#         data 
-# (bkg, 20)
-mean_rl, Dmean_rl = sky.mean_n_std(mean_lightdata,axis=1)
+# # for bkg in BKG_VALUES:
+# #     for i in range(BKG_ITER)
+# #         data 
+# # (bkg, 20)
+# mean_rl, Dmean_rl = sky.mean_n_std(mean_lightdata,axis=1)
 
-# mean_rl = np.mean(mean_lightdata,axis=1)
-diff = mean_rl - mean_sl
-for df, dm in zip(diff,Dmean_rl):
-    print(df,dm,dm/df*100,'%')
+# # mean_rl = np.mean(mean_lightdata,axis=1)
+# diff = mean_rl - mean_sl
+# for df, dm in zip(diff,Dmean_rl):
+#     print(df,dm,dm/df*100,'%')
 
-plt.figure()
-plt.errorbar(BKG_VALUES,diff,fmt='.--')
-plt.grid()
-plt.figure()
-plt.errorbar(BKG_VALUES,diff,fmt='.--')
-for i in range(BKG_ITER):
-    plt.errorbar(BKG_VALUES,mean_lightdata[:,i]-mean_sl,fmt='.')
-plt.show()
+# select = slice(-5,None)
+
+# plt.figure()
+# plt.errorbar(BKG_VALUES[select],diff[select],fmt='.--')
+# plt.grid(linestyle='dotted',alpha=0.7)
+# plt.xlabel('$\\bar{n}_B$',fontsize=FONTSIZE)
+# plt.ylabel('$\\langle\\ell\\rangle - \\langle\\ell_0\\rangle$',fontsize=FONTSIZE)
+# plt.figure()
+# plt.errorbar(BKG_VALUES,diff,fmt='.--')
+# for i in range(BKG_ITER):
+#     plt.errorbar(BKG_VALUES,mean_lightdata[:,i]-mean_sl,fmt='.')
+# plt.show()
