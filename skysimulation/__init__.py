60371c2a9ca3f35c0ab131778c326805141b7636
00-berni <bernardo.vettori@outlook.it>
2025-04-12 02:06:02 +0200

    dunder names

--- skysimulation/__init__.py
+++ skysimulation/__init__.py
@@ -5,6 +5,9 @@ from .field import from_parms_to_distr, BACK_SEED,BACK_MEAN,BACK_SIGMA
 from .field import atm_seeing, noise, SEEING_SIGMA, NOISE_SEED, NOISE_MEAN, NOISE_SIGMA
 from .restoration import bkg_est, LR_deconvolution,searching,kernel_estimation,light_recover
 
+__author__  = '00-berni'
+__version__ = '0.0.0'
+
 MASS_ENDS = [MIN_m, MAX_m]
 FRAME = {'size': N, 'stars': M, 'mass_seed': M_SEED, 'mass_range': MASS_ENDS, 'pos_seed': POS_SEED, 'norm': K}
 BACK  = {'seed': BACK_SEED, 'mean': BACK_MEAN, 'sigma': BACK_SIGMA}
